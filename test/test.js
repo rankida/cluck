3d77229922a795f139123db86d9417e0420034b2
David Rankin <Rankida@gmail.com>
2016-11-02 11:28:25 +0000

    Added a basic test to make sure all is working.

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+const assert = require('chai').assert;
+describe('Array', function () {
+  describe('#indexOf', function () {
+    it('should return -1 when the value is not presetnt', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(5));
+      assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+  });
+});
