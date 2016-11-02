48f47b0ef3b9f783329a073db82d59fc17c68f1c
David Rankin <Rankida@gmail.com>
2016-11-02 12:42:02 +0000

    Very basic hapi server. Have a look with `npm start`

---
+++ server.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+const server = new Hapi.Server();
+
+server.connection({ port: 3000 });
+
+server.register(require('inert'), (err) => {
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+      reply.file('./public/index.html');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/public/{param*}',
+    handler: {
+      directory: {
+        path: 'public',
+        listing: true
+      }
+    }
+  });
+});
+
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: (request, reply) => reply('Hello from Rankida')
+});
+
+server.route({
+  method: 'GET',
+  path: '/hello/{name}',
+  handler: (request, reply) => reply('Hello ' + encodeURIComponent(request.params.name))
+});
+
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
+    }
+  }
+}, (err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(`Server is running on port: ${server.info.uri}`);
+  });
+});
